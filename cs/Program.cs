3bb47b2f491844347c0972e50ec7e3c4133ce60c
Jouke van der Maas <jouke.vandermaas@gmail.com>
2020-12-26 13:19:36 +0100

    Add c# project

---
+++ cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ColorClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
